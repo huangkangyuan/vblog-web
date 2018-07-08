088904a1e4c90487de10d5e59d13dec478a15d86
liangfeihu <liangfhhd@163.com>
2018-07-08 18:12:51 +0800

    文章详情页展示

--- src/views/blog/BlogView.vue
+++ src/views/blog/BlogView.vue
@@ -19,9 +19,9 @@
             <div class="me-view-info">
               <span>{{article.author.nickname}}</span>
               <div class="me-view-meta">
-                <span>{{article.createDate | format}}</span>
-                <span>阅读   {{article.viewCounts}}</span>
-                <span>评论   {{article.commentCounts}}</span>
+                <span>{{article.createTime | format}}</span>
+                <span>阅读   {{article.viewNum}}</span>
+                <span>评论   {{article.commentNum}}</span>
               </div>
             </div>
             <el-button
@@ -48,13 +48,13 @@
           <div class="me-view-tag">
             标签：
             <!--<el-tag v-for="t in article.tags" :key="t.id" class="me-view-tag-item" size="mini" type="success">{{t.tagname}}</el-tag>-->
-            <el-button @click="tagOrCategory('tag', t.id)" size="mini" type="primary" v-for="t in article.tags" :key="t.id" round plain>{{t.tagname}}</el-button>
+            <el-button @click="tagOrCategory('tag', t.id)" size="mini" type="primary" v-for="t in article.tags" :key="t.id" round plain>{{t.tagName}}</el-button>
           </div>
 
           <div class="me-view-tag">
             文章分类：
             <!--<span style="font-weight: 600">{{article.category.categoryname}}</span>-->
-            <el-button @click="tagOrCategory('category', article.category.id)" size="mini" type="primary" round plain>{{article.category.categoryname}}</el-button>
+            <el-button @click="tagOrCategory('category', article.category.id)" size="mini" type="primary" round plain>{{article.category.categoryName}}</el-button>
           </div>
 
           <div class="me-view-comment">
@@ -125,13 +125,13 @@
         article: {
           id: '',
           title: '',
-          commentCounts: 0,
-          viewCounts: 0,
+          commentNum: 0,
+          viewNum: 0,
           summary: '',
+          createTime: '',
           author: {},
           tags: [],
           category:{},
-          createDate: '',
           editor: {
             value: '',
             toolbarsFlag: false,
@@ -176,7 +176,7 @@
         let that = this
         viewArticle(that.$route.params.id).then(data => {
           Object.assign(that.article, data.data)
-          that.article.editor.value = data.data.body.content
+          that.article.editor.value = data.data.content
 
           that.getCommentsByArticle()
         }).catch(error => {
